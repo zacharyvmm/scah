09db8b14bb5f041bb38a580625a777a117cf14f8
Zachary McSween Manickchand <zacharymcsweenmanickchand@gmail.com>
2025-06-30 18:24:35 -0400

    API design for Rust too now

--- examples/python/api_design.py
+++ examples/python/api_design.py
@@ -2,28 +2,30 @@
 # Anything in this document is subject to change.
 
 
-from scrooge import HtmlParser
-
-doc = HtmlParser(html="...")
+from scrooge import HtmlParser, Selectors
 
 # NOTE: Because of the nesting in the created by conditionals I would either need to
 # 1) Conditional FSM
 # 1.1) Using a tree
 # 1.2) Using a custom linear tree (foward polish notation style)
 # 1.3) Using Conditional Vec of Vec
+# 1.4) A other list with (1st item start index, 2nd item start index)s
 # 2) Nested FSM (Fsm within the FSM)
-doc.template = {
-    "key": doc.all("main > section", textContent=True).then(
+s = Selectors()
+s.template = {
+    "key": s.all("main > section", textContent=True).then(
         # To handle the `or` clause I would need to run write the `__or__` dunder to genereate a OR_CLAUSE object
         # Then I would run the lambda (returns OR_CLAUSE)
         lambda section: section.first("p", innerHtml=True)
             or section.all("a[href]", textContent=False)
             # The `textContent=False` would overide the `textContent=True` from before
     ),
-    "no concatination in css strings": doc.all("main").then(
-        lambda section: section.first("p") or section.all("a[href]"), concat="child"
+    "key 2": s.all("main").then(
+        lambda section: section.first("p") and section.all("a[href]"), concat="child"
     ),
 }
+
+doc = HtmlParser(html="...", selectors=s)
 
 
 doc.select("key")
@@ -49,3 +51,6 @@ doc.select("key")
 
 # Returns the whole dict
 doc.selections()
+
+# I should be able to unpack the selections for `and`
+p, a = doc.select("key 2")
