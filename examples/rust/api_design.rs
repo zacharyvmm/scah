3635785361ed715f87ddb4523df75d2e265eb25d
Zachary McSween Manickchand <zacharymcsweenmanickchand@gmail.com>
2025-07-05 18:13:02 -0400

    api design `.all` and `.first`. `.then` determined to only be for paths

--- examples/rust/api_design.rs
+++ examples/rust/api_design.rs
@@ -1,32 +1,27 @@
 // NOTE: This is a direct translation from the `api_design.py` file
 
-use scrooge::{Condition /* ENUM */, HtmlParser, InnerContent, Selectors};
+use scrooge::{HtmlParser, InnerContent, Selectors, Reader};
 
 fn main() {
     let mut s = Selectors::new();
     s.add(
         "key",
-        s.all("main > section").then(|section| {
-            // Instead of Condition::Or I could do the same thing I do with python and implement the BitOr Trait
-            // https://doc.rust-lang.org/core/ops/trait.BitOr.html
-            // BUT: I don't think this kind of operator overloading is appreciated by rust users
-            Condition::Or(
-                section.first(
-                    "p",
-                    InnerContent {
-                        inner_html: true,
-                        text_content: false,
-                    },
-                ),
-                section.all(
-                    "a[href]",
-                    InnerContent {
-                        inner_html: false,
-                        text_content: false,
-                    },
-                ),
-            )
-        }),
+        s.all("main > section").first("div").or(
+            Selector::new().first(
+                Reader::new("p"),
+                InnerContent {
+                    inner_html: true,
+                    text_content: false,
+                },
+            ),
+            Selector::new().all(
+                Reader::new("a[href]"),
+                InnerContent {
+                    inner_html: false,
+                    text_content: false,
+                },
+            ),
+        )
     );
 
     // For the same of efficiency I would like to be able to pass a immutable reference to the parser.
