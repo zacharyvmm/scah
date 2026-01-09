296ffb12b48cbb48a201116ef481063266f7fe19
Zachary McSween Manickchand <zacharymcsweenmanickchand@gmail.com>
2026-01-08 19:31:23 -0500

    no first selection in a all selection benchmark

--- benches/simple/all/speed_bench.rs
+++ benches/simple/all/speed_bench.rs
@@ -51,23 +51,6 @@ fn bench_comparison(c: &mut Criterion) {
         //     },
         // );
 
-        group.bench_with_input(
-            BenchmarkId::new("onego_first_element", size),
-            &content,
-            |b, html| {
-                b.iter(|| {
-                    let queries = &[Query::first(black_box(QUERY), Save::all()).build()];
-
-                    let res = parse(&html, queries);
-
-                    let element = res[QUERY].value().unwrap();
-                    black_box(&element.attributes);
-                    black_box(&element.inner_html);
-                    black_box(&element.text_content);
-                })
-            },
-        );
-
         group.bench_with_input(BenchmarkId::new("tl", size), &content, |b, html| {
             b.iter(|| {
                 let dom = tl::parse(html, ParserOptions::default()).unwrap();
@@ -85,25 +68,6 @@ fn bench_comparison(c: &mut Criterion) {
             })
         });
 
-        group.bench_with_input(
-            BenchmarkId::new("tl_first_element", size),
-            &content,
-            |b, html| {
-                b.iter(|| {
-                    let dom = tl::parse(html, ParserOptions::default()).unwrap();
-                    let parser = dom.parser();
-                    let node_handle = dom.query_selector(QUERY).unwrap().next().unwrap();
-
-                    if let Some(node) = node_handle.get(parser) {
-                        let attributes = node.as_tag().unwrap().attributes();
-                        black_box(attributes.get("href"));
-                        black_box(node.inner_html(parser));
-                        black_box(node.inner_text(parser));
-                    }
-                })
-            },
-        );
-
         group.bench_with_input(BenchmarkId::new("scraper", size), &content, |b, html| {
             b.iter(|| {
                 let document = Html::parse_document(html);
@@ -116,22 +80,6 @@ fn bench_comparison(c: &mut Criterion) {
                 }
             })
         });
-
-        group.bench_with_input(
-            BenchmarkId::new("scraper_first_element", size),
-            &content,
-            |b, html| {
-                b.iter(|| {
-                    let document = Html::parse_document(html);
-                    let selector = Selector::parse(QUERY).unwrap();
-
-                    let element = document.select(&selector).next().unwrap();
-                    black_box(element.attr("href"));
-                    black_box(element.inner_html());
-                    black_box(element.text().collect::<Vec<&str>>());
-                })
-            },
-        );
 
         group.bench_with_input(BenchmarkId::new("lexbor", size), &content, |b, html| {
             b.iter(|| {
@@ -146,23 +94,6 @@ fn bench_comparison(c: &mut Criterion) {
                 }
             })
         });
-
-        group.bench_with_input(
-            BenchmarkId::new("lexbor_first_element", size),
-            &content,
-            |b, html| {
-                b.iter(|| {
-                    let doc = HtmlDocument::new(html.as_str()).expect("Failed to parse HTML");
-                    let nodes = doc.select(QUERY);
-
-                    let node = nodes.iter().next().unwrap();
-                    // TODO: I need to add attributes and innerhtml for lexbor
-                    black_box(node.text_content());
-                    black_box(node.inner_html());
-                    black_box(node.attributes());
-                })
-            },
-        );
     }
     group.finish();
 }
