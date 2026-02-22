531d803ef86353b84202189b7dc22327f641f0d6
Zachary McSween Manickchand <zacharymcsweenmanickchand@gmail.com>
2026-02-22 18:37:01 -0500

    Factory wasn't working because of napi-rs BUG.
    If you have an empty struct (equivalent object: `{}`) it defaults to a value of undefined.

--- nodejs/__test__/index.spec.ts
+++ nodejs/__test__/index.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from 'bun:test'
 
-import { parse, Query, Save } from '../index'
+import { parse, Query } from '../index'
 
 test('Basic selection', () => {
   const html = `
@@ -14,6 +14,67 @@ test('Basic selection', () => {
   const store = parse(Buffer.from(html), [query]);
 
   expect(store.length).toBe(3);
+
+  expect(store.get(0)?.children).toEqual([
+    [Buffer.from('div'), [1]],
+  ])
+
+  expect(store.get(1)).toEqual({
+    name: Buffer.from('div'),
+    class: undefined,
+    id: undefined,
+    attributes: [],
+    innerHtml: Buffer.from(`
+    Hello World
+    <a href="https://example.com">Example Website</a>
+  `),
+    textContent: Buffer.from('Hello World Example Website'),
+    children: [[Buffer.from('a'), [2]]],
+  })
+
+  expect(store.get(2)).toEqual({
+    name: Buffer.from('a'),
+    class: undefined,
+    id: undefined,
+    attributes: [[Buffer.from('href'), Buffer.from('https://example.com')]],
+    innerHtml: Buffer.from(`Example Website`),
+    textContent: Buffer.from('Example Website'),
+    children: [],
+  })
+})
+
+test('Tree selection', () => {
+  const html = `
+  <section id="products">
+    <div class="product">
+      <h1>Product #1</h1>
+      <img src="https://example.com/p1.png"/>
+      <p>
+        Hello World for Product #1
+      </p>
+    </div>
+    
+    <div class="product">
+      <h1>Product #2</h1>
+      <img src="https://example.com/p2.png"/>
+      <p>
+        Hello World for Product #2
+      </p>
+    </div>
+
+  </section>
+  `;
+  const query = Query.all('#products', {innerHtml: true, textContent: true})
+    .all('.product', {innerHtml: true, textContent: true})
+    .then(p => {return [
+      p.first('h1', {innerHtml: true, textContent: true}),
+      p.first('img', {innerHtml: false, textContent: false}),
+      p.first('p', {innerHtml: true, textContent: true}),
+    ]}).build();
+  console.log(query.toString())
+  const store = parse(Buffer.from(html), [query]);
+
+  expect(store.length).toBe(9);
 
   expect(store.get(0)?.children).toEqual([
     [Buffer.from('div'), [1]],
